1bb9c32deba6541288313004d35e38d539fd513c
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-02-25 03:19:16 -0800

    Add input prompts for starting and ending numbers in Multiplication Table Generator

--- Program.cs
+++ Program.cs
@@ -2,6 +2,10 @@
 {
     Console.WriteLine("Multiplication Table Generator");
     Console.WriteLine("".PadLeft(30, '-'));
+    Console.Write("Enter the starting number: ");
+    var startNumber = Console.ReadLine();
+    Console.Write("Enter the ending number: ");
+    var endNumber = Console.ReadLine();
 }
 catch (FormatException e)
 {
